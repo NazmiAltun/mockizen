6fde719237a6bd4ba292971bad06b9b749e88417
Nazmi Altun <nazmialtun@windowslive.com>
2020-09-17 14:19:55 +0800

    no need teardown

--- src/test/teardown.ts
+++
@@ -1,7 +0,0 @@
-import { createHttpTerminator } from 'http-terminator';
-import server from '..';
-
-after(async () => {
-  const httpTerminator = createHttpTerminator({ server });
-  await httpTerminator.terminate();
-});
